a08adc7ead94201dda1f75e0eff9fe8e708148ef
cyrusjameszarsuelo <james.zarsuelo@indigo21.com>
2025-03-28 19:31:21 +0800

    property inspector index
    Added datatables, propoerty inspector index

--- resources/views/includes/aside.blade.php
+++ resources/views/includes/aside.blade.php
@@ -140,7 +140,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
+                <a href="{{route('property-inspector.index')}}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Property Inspector Configuration</p>
                 </a>
