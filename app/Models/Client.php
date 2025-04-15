72710fc0b998cbc4fc9791505a4074934df3905a
Charles-indigo21 <charles.verdadero@indigo21.com>
2025-04-16 00:49:35 +0800

    Client Configuration Model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -23,4 +23,29 @@ class Client extends Model
     public function chargingScheme(): BelongsTo {
         return $this->belongsTo(ChargingScheme::class);
     }
+
+    public static function getClientData(){
+        $result_data = self::select(['clients.id as client_id',
+                                    'users.id as client_user_id',
+                                    'users.firstname as client_name',
+                                    'users.email as client_email',
+                                    'users.mobile as client_mobile',
+                                    'clients.client_abbrevation as client_tla',
+                                    'clients.address1 as client_address1',
+                                    'clients.address2 as client_address2',
+                                    'clients.address3 as client_address3',
+                                    'clients.city as client_city',
+                                    'clients.country as client_country',
+                                    'clients.postcode as client_postcode',
+                                    'client_types.id as client_type_id',
+                                    'client_types.name as client_type',
+                                    'client_key_details.is_active as is_active',]);
+        $result_data->leftJoin('users', 'clients.user_id', '=', 'users.id');
+        $result_data->leftJoin('client_types', 'clients.client_type_id', '=', 'client_types.id');
+        $result_data->leftJoin('client_key_details', 'clients.id', '=', 'client_key_details.client_id');
+        
+        return $result_data->get();
+    }
+
+    
 }
