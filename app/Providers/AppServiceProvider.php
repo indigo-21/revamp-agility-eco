9309979d5ec14e65649a748adf0ae95f7a498b35
cyrusjameszarsuelo <james.zarsuelo@indigo21.com>
2025-07-18 22:42:15 +0800

    Revert: Add AppServiceProvider.php back to tracking and remove from .gitignore

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Navigation;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
+        View::composer('*', function ($view) {
+            $user = auth()->user();
+
+            if ($user === null) {
+                return;
+            }
+
+            $navigations = Navigation::whereHas('userNavigations', function ($q) use ($user) {
+                $q->where('account_level_id', $user->accountLevel->id);
+            })->with([
+                        'userNavigations' => function ($q) use ($user) {
+                            $q->where('account_level_id', $user->accountLevel->id);
+                        }
+                    ])->get();
+
+
+            $currentLink = request()->segment(1);
+            $userPermission = $navigations
+                ->where('link', $currentLink)
+                ->first()
+                ?->userNavigations
+                ->first()
+                    ?->permission ?? 1;
+
+            $view->with('navigations', $navigations)
+                ->with('userPermission', $userPermission);
+        });
+    }
+}
