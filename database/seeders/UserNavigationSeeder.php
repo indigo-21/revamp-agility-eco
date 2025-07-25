f666aa7a5d5dfb2d624af5540d36f29f8d867085
cyrusjameszarsuelo <james.zarsuelo@indigo21.com>
2025-07-25 23:46:16 +0800

    Seeders

--- database/seeders/UserNavigationSeeder.php
+++ database/seeders/UserNavigationSeeder.php
@@ -15,7 +15,7 @@ class UserNavigationSeeder extends Seeder
     {
         $values = [];
         for ($i = 1; $i <= 38; $i++) {
-            if ($i != 33 && $i != 38 && $i != 39 ) {
+            if (!in_array($i, [2, 34, 40, 41])) {
                 $values[] = [
                     'account_level_id' => 1,
                     'navigation_id' => $i,
@@ -26,71 +26,53 @@ class UserNavigationSeeder extends Seeder
 
         $values[] = [
             'account_level_id' => 5,
-            'navigation_id' => 42,
+            'navigation_id' => 2,
             'permission' => 3,
         ];
         $values[] = [
             'account_level_id' => 5,
-            'navigation_id' => 30,
+            'navigation_id' => 31,
             'permission' => 1,
         ];
         $values[] = [
             'account_level_id' => 5,
-            'navigation_id' => 33,
+            'navigation_id' => 34,
             'permission' => 3,
         ];
 
         $values[] = [
             'account_level_id' => 6,
-            'navigation_id' => 38,
+            'navigation_id' => 40,
             'permission' => 3,
         ];
 
         $values[] = [
             'account_level_id' => 7,
-            'navigation_id' => 38,
+            'navigation_id' => 40,
             'permission' => 3,
         ];
 
         $values[] = [
             'account_level_id' => 8,
-            'navigation_id' => 38,
+            'navigation_id' => 40,
             'permission' => 3,
         ];
 
         $values[] = [
             'account_level_id' => 6,
-            'navigation_id' => 39,
+            'navigation_id' => 41,
             'permission' => 3,
         ];
 
         $values[] = [
             'account_level_id' => 7,
-            'navigation_id' => 39,
+            'navigation_id' => 41,
             'permission' => 3,
         ];
 
         $values[] = [
             'account_level_id' => 8,
-            'navigation_id' => 39,
-            'permission' => 3,
-        ];
-
-        $values[] = [
-            'account_level_id' => 6,
-            'navigation_id' => 26,
-            'permission' => 3,
-        ];
-
-        $values[] = [
-            'account_level_id' => 7,
-            'navigation_id' => 26,
-            'permission' => 3,
-        ];
-
-        $values[] = [
-            'account_level_id' => 8,
-            'navigation_id' => 26,
+            'navigation_id' => 41,
             'permission' => 3,
         ];
 
@@ -127,6 +109,24 @@ class UserNavigationSeeder extends Seeder
         $values[] = [
             'account_level_id' => 8,
             'navigation_id' => 28,
+            'permission' => 3,
+        ];
+
+        $values[] = [
+            'account_level_id' => 6,
+            'navigation_id' => 29,
+            'permission' => 3,
+        ];
+
+        $values[] = [
+            'account_level_id' => 7,
+            'navigation_id' => 29,
+            'permission' => 3,
+        ];
+
+        $values[] = [
+            'account_level_id' => 8,
+            'navigation_id' => 29,
             'permission' => 3,
         ];
 
